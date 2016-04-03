e789d2713b3942c9d44bd5c104c045281d8b919c
Anton Bezrukov <hello@antonbezrukov.ru>
2016-04-03 05:20:22 +0300

    Deleted old and not actual comments

--- db.go
+++ db.go
@@ -6,10 +6,8 @@ import (
 	_ "github.com/lib/pq" // The driver for Postgres.
 )
 
-// DB extends sql.DB type with methods related to a multi-tenant use case.
 var DB *sql.DB
 
-// Use ...
 func Use(schemaName string, tx *sql.Tx) (*sql.Tx, error) {
 
 	_, err := tx.Exec("SET search_path = " + schemaName)
@@ -21,7 +19,6 @@ func Use(schemaName string, tx *sql.Tx) (*sql.Tx, error) {
 	return tx, nil
 }
 
-// Open wraps sql.Open function and returns pointer on *DB instead of *sql.DB.
 func Open(connectionParams string) (*sql.DB, error) {
 	DB, err := sql.Open("postgres", connectionParams) // Boat uses Postgres only.
 
